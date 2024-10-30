9b851241d5773d889ec5a47190ddfbfe21e7a665
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-10-30 21:13:10 +0800

    connect mysql to node.js successfully

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require("./dbConfig");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is up and running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
