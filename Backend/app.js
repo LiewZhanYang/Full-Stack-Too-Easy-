5f6c04ee93185461e1b049b3eaf53760232778c5
Liew Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2024-10-31 12:00:13 +0800

    finish implement crud for program and tested in postman successfully

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const db = require("./dbConfig");
-const customerRoutes = require('./routes/customerRoutes');
+const customerRoutes = require("./routes/customerRoutes");
+const programRoutes = require("./routes/programRoutes");
 const app = express();
 
 app.use(express.json());
 
-app.use('/api',customerRoutes);
+app.use("/api", customerRoutes);
+app.use('/api', programRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Backend is up and running!");
