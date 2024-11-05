35e5679bdaeac08ba26f092e652d17688cd43683
Shskjssk10 <cadentohjunyi@gmail.com>
2024-11-05 21:48:33 +0800

    Changed var name

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createPool({
+const dbConfig = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -10,4 +10,4 @@ const db = mysql.createPool({
 });
 
 
-module.exports = db;
+module.exports = dbConfig;
