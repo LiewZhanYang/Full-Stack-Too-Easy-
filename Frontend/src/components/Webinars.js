250951109a2d2b0f003e16091a60df92547d4ef3
moni <monishacelestina123@gmail.com>
2024-11-08 09:35:20 +0800

    Removed duplicate sidebar

--- Frontend/src/components/Webinars.js
+++ Frontend/src/components/Webinars.js
@@ -4,7 +4,6 @@ import Sidebar from './Sidebar';
 function ComponentName() {  
   return (  
     <div className="flex min-h-screen bg-gray-50">  
-      <Sidebar />  
       <div className="flex-1 p-8">  
         <h1>ComponentName Page</h1>  
         {/* Component content here */}  
