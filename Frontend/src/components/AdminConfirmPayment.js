9bcf01ea334cc48a46a80359a09b7acc713c5752
moni <monishacelestina123@gmail.com>
2024-11-15 05:58:45 +0800

    Updated admin text

--- Frontend/src/components/AdminConfirmPayment.js
+++ Frontend/src/components/AdminConfirmPayment.js
@@ -3,7 +3,7 @@ import { Container, Row, Col, Button, Form, Modal } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 
-const AdminConfirmPayment = () => {
+const AdminConfirmPayment = () => { // 
   const [paymentDetails, setPaymentDetails] = useState({});
   const [rejectionReason, setRejectionReason] = useState("");
   const [showConfirmModal, setShowConfirmModal] = useState(false);
