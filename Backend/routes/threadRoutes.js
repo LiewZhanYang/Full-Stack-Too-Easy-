809208b5b9448ae49ed20b97bcc26e1d922a6b00
MonishaCelestina <s10220984@connect.np.edu.sg>
2025-01-28 05:10:44 +0800

    Editing backend for forum

--- Backend/routes/threadRoutes.js
+++ Backend/routes/threadRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const threadController = require("../controllers/threadController");
 
 router.get("/", threadController.getThreads);
-router.get("/:id", threadController.getCommentByThreadID);
+router.get("/:id/comment", threadController.getCommentByThreadID)
 router.post("/comment", threadController.postComment);
 router.post("/thread", threadController.postThread);
 router.put("/like/:id", threadController.likeThread);
 router.put("/dislike/:id", threadController.dislikeThread);
+router.get("/:id", threadController.getThreadByID);
 
 module.exports = router;
